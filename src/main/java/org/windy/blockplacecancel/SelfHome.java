9369cad6f070cf92bae38a504450bc7f295efa90
风吟 <2580855214@qq.com>
2024-02-18 10:35:33 +0800

    重写精准检测

--- src/main/java/org/windy/blockplacecancel/SelfHome.java
+++ src/main/java/org/windy/blockplacecancel/SelfHome.java
@@ -1,144 +1,112 @@
 package org.windy.blockplacecancel;
 
-import com.SelfHome.Main;
 import com.SelfHome.Variable;
 import com.Util.Home;
 import com.Util.HomeAPI;
 import com.Util.Util;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.configuration.Configuration;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
 public class SelfHome {
 
     public static void executeIfSh(BlockPlaceEvent event, Configuration config) {
-        Player p = event.getPlayer(); // 获取触发事件的玩家
-        // 使用传入的配置对象获取配置信息
-        List<String> TileList = config.getStringList("SelfHomeMain.TileList");
-        boolean EnableClearExtraBlocks = config.getBoolean("SelfHomeMain.EnableClearExtraBlocks");
-
-        if (Variable.Debug.contains(p.getName())) {
-            p.sendMessage("§e§l§m--------------§7[§eDeBug§7]§e§l§m--------------");
-            TextComponent Send_Block_Message = new TextComponent(
-                    "§eGet-Returned:§d" + Util.getNBTString(event.getBlock().getState()) + " §b>> §dCopy");
-            Send_Block_Message.setClickEvent(
-                    new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, Util.getNBTString(event.getBlock().getState())));
-            p.spigot().sendMessage((BaseComponent)Send_Block_Message);
-            p.sendMessage("§e§l§m--------------§7[§eDeBug§7]§e§l§m--------------");
-        }
-
-        if (!Util.CheckIsHome(event.getPlayer().getWorld().getName().replace(Variable.world_prefix, ""))) {
-            return;
-        }
-        
+        Player player = event.getPlayer();
         Block block = event.getBlock();
-        String nbt = Util.getNBTString(block.getState());
-        boolean check_contain = false;
-        String contain_nbt = "";
-        int MaxThisTile = 0;
-        for (String tile : TileList) {
-            String[] temp = tile.split("\\|");
-            if (temp[0].equalsIgnoreCase("chunk") && nbt.equalsIgnoreCase(temp[1])) {
-                check_contain = true;
-                contain_nbt = temp[1];
-                MaxThisTile = Integer.parseInt(temp[2]);
-                break;
-            }
-        }
+        String worldName = block.getWorld().getName().replace(Variable.world_prefix, "");
 
-        if (!check_contain) {
+        // 获取配置文件中的 TileList.chunk 和 TileList.world 配置项
+        List<String> tileListChunk = config.getStringList("SelfHomeMain.TileList.chunk");
+        List<String> tileListWorld = config.getStringList("SelfHomeMain.TileList.world");
+
+        // 检查 TileList.chunk 中的配置
+        if (isBlocked(block, tileListChunk, "chunk")) {
+            event.setCancelled(true);
+            player.sendMessage(Variable.Lang_YML.getString("PlaceReachMaxTile"));
             return;
         }
-        int NowAmount = 0;
 
-
-        boolean extra_perm = false;
-        int extra_amount = MaxThisTile;
-        for (int i = 100; i > 0; i--) {
-            if (p.hasPermission("SelfHome.ChunkPlace." + contain_nbt + "." + i)) {
-                extra_perm = true;
-                if (extra_amount < i) {
-                    extra_amount = i;
-                    break;
-                }
-            }
-        }
-
-
-        Home home = HomeAPI.getHome(event.getBlock().getWorld().getName().replace(Variable.world_prefix, ""));
-        for(String str:home.getLimitBlock()) {
-            String[] args = str.split("\\|");
-            if(args[0].equalsIgnoreCase("chunk") && nbt.toUpperCase().contains(args[1].toUpperCase())) {
-                int amount = Integer.valueOf(args[2]);
-                if(extra_amount < amount) {
-                    extra_perm = true;
-                    extra_amount = amount;
-                }
-            }
-        }
-
-
-
-
-        int b;
-        int j;
-        BlockState[] arrayOfBlockState;
-        for (j = (arrayOfBlockState = event.getBlock().getChunk().getTileEntities()).length, b = 0; b < j; ) { BlockState state = arrayOfBlockState[b];
-            if (Util.getNBTString(state).toUpperCase().contains(contain_nbt.toUpperCase())) {
-                NowAmount++;
-
-                if (EnableClearExtraBlocks &&
-                        NowAmount > MaxThisTile) {
-                    event.setCancelled(true);
-                    state.getBlock().getLocation().getWorld().getBlockAt(state.getBlock().getLocation()).setType(Material.AIR);
-                    p.sendMessage(Variable.Lang_YML.getString("ClearExtraBlocks"));
-                }
-            }
-
-            b++; }
-
-        NowAmount--;
-
-
-        if (extra_perm) {
-            MaxThisTile = extra_amount;
-        }
-
-        if (NowAmount + 1 <= MaxThisTile) {
-            String temp = Variable.Lang_YML.getString("PlaceMaxTile");
-            if (temp.contains("<Now>")) {
-                temp = temp.replace("<Now>", String.valueOf(NowAmount + 1));
-            }
-            if (temp.contains("<Max>")) {
-                temp = temp.replace("<Max>", String.valueOf(MaxThisTile));
-            }
-            if (temp.contains("<NBT>")) {
-                temp = temp.replace("<NBT>", String.valueOf(contain_nbt));
-            }
-            p.sendMessage(temp);
-        } else {
-            String temp = Variable.Lang_YML.getString("PlaceReachMaxTile");
-            if (temp.contains("<Now>")) {
-                temp = temp.replace("<Now>", String.valueOf(NowAmount));
-            }
-            if (temp.contains("<Max>")) {
-                temp = temp.replace("<Max>", String.valueOf(MaxThisTile));
-            }
-            if (temp.contains("<NBT>")) {
-                temp = temp.replace("<NBT>", String.valueOf(contain_nbt));
-            }
+        // 检查 TileList.world 中的配置
+        Home home = HomeAPI.getHome(worldName);
+        if (home != null && isBlocked(block, tileListWorld, "world")) {
             event.setCancelled(true);
-            p.sendMessage(temp);
+            player.sendMessage(Variable.Lang_YML.getString("PlaceReachMaxTile"));
         }
+    }
+
+    /**
+     * 检查玩家是否被阻止在特定方块上放置更多方块
+     * @param block 放置的方块
+     * @param tileList 方块配置列表
+     * @param type 方块类型
+     * @return 如果放置的方块数量超过了配置的最大值，则返回 true；否则返回 false
+     */
+    private static boolean isBlocked(Block block, List<String> tileList, String type) {
+        // 获取放置方块的 NBT 数据
+        String nbt = Util.getNBTString(block.getState());
+        // 遍历方块配置列表
+        for (String tile : tileList) {
+            // 分割配置项
+            String[] temp = tile.split("\\|");
+            // 检查配置项格式是否正确
+            if (temp.length >= 2 && nbt.equalsIgnoreCase(temp[0])) {
+                // 获取配置的最大放置数量
+                int maxAmount = Integer.parseInt(temp[1]);
+                // 获取当前放置的方块数量
+                int currentAmount = countPlacedBlocks(block, type, temp[0]);
+                // 如果当前数量超过了最大数量，则返回 true
+                if (currentAmount >= maxAmount) {
+                    return true;
+                }
+            }
+        }
+        // 如果未超过最大数量，则返回 false
+        return false;
+    }
+
+    /**
+     * 计算玩家在特定方块周围已经放置的方块数量
+     * @param block 放置的方块
+     * @param type 方块类型（"chunk" 或 "world"）
+     * @param nbt 方块的 NBT 数据
+     * @return 放置的方块数量
+     */
+    private static int countPlacedBlocks(Block block, String type, String nbt) {
+        // 初始化放置方块数量为 0
+        int count = 0;
+        // 如果方块类型为 "chunk"
+        if (type.equalsIgnoreCase("chunk")) {
+            // 遍历方块所在区块的所有方块状态
+            for (BlockState state : block.getChunk().getTileEntities()) {
+                // 检查方块状态的 NBT 数据是否包含指定的 NBT 数据
+                if (Util.getNBTString(state).toUpperCase().contains(nbt.toUpperCase())) {
+                    // 如果包含，则计数加一
+                    count++;
+                }
+            }
+        }
+        // 如果方块类型为 "world"
+        else if (type.equalsIgnoreCase("world")) {
+            // 获取方块所在世界的家园信息
+            Home home = HomeAPI.getHome(block.getWorld().getName().replace(Variable.world_prefix, ""));
+            // 如果家园信息不为空
+            if (home != null) {
+                // 遍历家园的限制方块列表
+                for (String str : home.getLimitBlock()) {
+                    // 分割限制方块信息
+                    String[] args = str.split("\\|");
+                    // 检查限制方块信息是否符合条件，并计数加一
+                    if (args.length >= 2 && args[0].equalsIgnoreCase("chunk") && nbt.toUpperCase().contains(args[1].toUpperCase())) {
+                        count++;
+                    }
+                }
+            }
+        }
+        // 返回放置方块数量
+        return count;
     }
 }
